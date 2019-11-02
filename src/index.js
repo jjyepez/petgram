d00fce8ace0057b44f6c70c34a884d7f82a43c4a
jjyepez <jjyepez@gmail.com>
2019-11-02 01:10:19 -0500

    Clase 5: Linter, extensiones y deploy con Now

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const app = document.getElementById('app');
-ReactDOM.render(<h1>It works!</h1>, app);
+const app = document.getElementById('app')
+ReactDOM.render(<h1>It works!</h1>, app)
