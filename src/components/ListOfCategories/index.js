8f05ddc15855a05b44b0c4888d9bb40324bb05f0
jjyepez <jjyepez@gmail.com>
2019-11-03 00:25:58 -0500

    Clase 9: Usar información real de las categorías

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,12 +1,15 @@
 import React from 'react'
+
 import { Category } from '../Category'
 import { List, Item } from './styles'
+
+import { categories } from '../../../api/db.json'
 
 export const ListOfCategories = (props) => {
   return (
     <List>
       {
-        [1, 2, 3, 4].map((category, i) => <Item key={i}><Category /></Item>)
+        categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
       }
     </List>
   )
